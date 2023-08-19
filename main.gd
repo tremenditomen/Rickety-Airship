df53a90231f0cc2d90f8f82655b97e346f9ca29a
Luis Andres Contreras <32722767+tremenditomen@users.noreply.github.com>
2023-08-18 21:58:47 -0600

    creating health bars
    little progress has been made

--- main.gd
+++ main.gd
@@ -1,11 +1,24 @@
-extends Node
+extends Node2D
+const MAX_HEALTH = 10
+var health = MAX_HEALTH
 
+func _ready() -> void:
+	set_health_label ()
+	$Health
+	set_health_bar ()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func set_health_bar () -> void:
+	
+func set_health_label() -> void:
+	$HealthLabel.text = "Health: %s" % health
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		damage()
+		
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func damage()-> void:
+	health -=1
+	if health<0:
+		health = MAX_HEALTH
+	set_health_label()
